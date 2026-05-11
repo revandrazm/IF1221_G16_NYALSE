562ab1800968ba4ae0641d0ecf8fa5483f5d8680
revandrazm <mahartarevandra@gmail.com>
2026-05-12 01:38:40 +0700

    feat: add aksiPendukung facts

--- src/lihatCommand.pl
+++ src/lihatCommand.pl
@@ -3,3 +3,7 @@ aksiUtama(ambilKartu).
 aksiUtama(tantang).
 aksiUtama(uni(indexKartu)).
 aksiUtama(tangkap(namaPemain)).
+
+aksiPendukung(lihatCommand).
+aksiPendukung(lihatKartu).
+aksiPendukung(cekInfo).
