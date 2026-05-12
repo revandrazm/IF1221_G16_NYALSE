80bad5e2bdc6bdf17683af23b2e2f684f2edafe6
Muhammad Abduh <mabduh1357@gmail.com>
2026-05-12 20:01:38 +0700

    refactor : mengubah load kartu dan shuffe agar tidak menggunakan fungsi bawaan, feat : menambahkan fitur bagi kartu, dan inisialisasi discard pile

--- src/deck.pl
+++ src/deck.pl
@@ -1,8 +1,81 @@
 :- include('facts.pl').
 
-loadKartu(ListKartu):- (findall(kartu(W, J), kartu(W, J), ListKartu)).
+listAppend([], X, X).
+listAppend([H|T], X, [H|TResult]) :- listAppend(T, X, TResult).
 
-shuffle(Awal, Hasil) :-
-    random_permutation(Awal, Hasil).
+getElement([E|_], 0, E).
+getElement([_|T], I, Element) :-
+    I > 0,
+    I1 is I - 1,
+    getElement(T, I1, Element).
 
+getLenght([], 0).
+getLenght([_|T], L) :-
+    getLenght(T, L1),
+    L is L1 + 1.
+
+deleteElement([_|T], 0, T).
+deleteElement([E|T], I, [E|TNew]) :-
+    I > 0,
+    I1 is I - 1,
+    deleteElement(T, I1, TNew).
+
+insertAt(TAwal, 0, E, [E|TAwal]).
+insertAt([H|T], I, E, [H|TResult]):-
+    I > 0,
+    I1 is I - 1,
+    insertAt(T, I1, E, TResult).
+
+kombinasiSatuWarna(_, [], []).
+kombinasiSatuWarna(Warna, [Jenis|SisaJenis], [kartu(Warna, Jenis)|BagianHasil]) :-
+    kombinasiSatuWarna(Warna, SisaJenis, BagianHasil).
+
+kombinasiSemuaWarna([], _, []).
+kombinasiSemuaWarna([Warna|SisaWarna], Jenis, HasilAkhir) :-
+    kombinasiSatuWarna(Warna, Jenis, HasilSatuWarna),
+    kombinasiSemuaWarna(SisaWarna, Jenis, HasilSementara),
+    listAppend(HasilSementara, HasilSatuWarna, HasilAkhir).
+
+% Convert fakta kartu ke list
+loadKartu(ListKartu):-
+    warnaDasar(ListWarna),
+    jenisKartu(ListJenis),
+    kartuHitam(ListKartuHitam),
+    kombinasiSemuaWarna(ListWarna, ListJenis, ListKartuWarna),
+    listAppend(ListKartuHitam, ListKartuWarna, ListKartu).
+
+shuffle([], []).
+shuffle(Awal, [ElemenAcak|SisaAcak]) :-
+    getLenght(Awal, Panjang),
+    random(0, Panjang, Index),
+    getElement(Awal, Index, ElemenAcak),
+    deleteElement(Awal, Index, AwalSisa),
+    shuffle(AwalSisa, SisaAcak).
+
+bagiKartu([], Deck, Deck).
+bagiKartu([Pemain|SisaPemain], Deck, SisaDeck):-
+    ambilKartu(KartuPemain, 7, Deck, DeckSementara),
+    assertz(kartu_pemain(Pemain, KartuPemain)),
+    bagiKartu(SisaPemain, DeckSementara, SisaDeck).
     
+ambilKartu([], 0, Deck, Deck) :- !.
+ambilKartu([H|TAmbil], N, [H|TDeck], SisaDeck) :-
+    N > 0,
+    N1 is N - 1,
+    ambilKartu(TAmbil, N1, TDeck, SisaDeck).
+
+initDiscard([kartu(Warna, Jenis)|Sisa], SisaAkhir):-
+    Warna \= hitam, !,
+    SisaAkhir = Sisa,
+    assertz(discard_top(kartu(Warna, Jenis))),
+    assertz(warna_aktif(Warna)),
+    write('Kartu awal di discard pile: '), write(kartu(Warna, Jenis)), nl.
+initDiscard([kartu(Warna, Jenis)|Sisa], SisaAkhir):-
+    getLenght(Sisa, Panjang),
+    random(0, Panjang, Index),
+    insertAt(Sisa, Index, kartu(Warna, Jenis), DeckBaru),
+    initDiscard(DeckBaru, SisaAkhir).
+
+printKartu :-
+	loadKartu(X), write(X).
+
