f02d79eae8330cafe78175a6a9feda1c14bbac6d
headns <betterheadns@gmail.com>
2026-05-12 11:49:06 +0700

    fix: discard top working

--- src/aksi_pendukung.pl
+++ src/aksi_pendukung.pl
@@ -4,14 +4,21 @@
 /* Helper */
 formatCard(kartu(Warna,Jenis)):-
 	format('~w-~w',[Warna, Jenis]).
-formatCard(Warna, Jenis) :-
-	format('~w-~w',[Warna, Jenis]).
 
 lihatHelper([], _) :- !.
-lihatHelper([[Warna, Jenis]|T], N) :-
-	format('~d. ', [N]), formatCard(Warna, Jenis), nl,
+lihatHelper([kartu(Warna,Jenis)|T], N) :-
+	format('~d. ', [N]), formatCard(kartu(Warna, Jenis)), nl,
 	N2 is N+1,
 	lihatHelper(T,N2).
+
+formatUrutanHelp([]) :- !.
+formatUrutanHelp([H|T]) :-
+	write(' - '), write(H),
+	formatUrutan(T).
+
+formatUrutan([H|T]):-
+	write(H),
+	formatUrutanHelp(T).
 
 /*
 printUrutan([], _) :- .
@@ -25,7 +32,7 @@ lihatKartu :-
 	lihatHelper(Deck,1).
 
 cekInfo :-
-	asserta(discard_top(kartu('hitam',wildcard'))),
 	discard_top(Top), urutan_pemain(Urutan), arah_permainan(Arah),
 	reverse(Urutan,UrutanRev),
-	write('Kartu discard top: '), formatCard(Top).
+	write('Kartu discard top: '), formatCard(Top), nl, nl,
+	write('Urutan pemain: '), formatUrutan(Urutan), nl, nl.
