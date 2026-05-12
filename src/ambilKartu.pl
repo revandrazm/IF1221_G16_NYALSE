ca62579616d89f9755e94f3a18e5bc4f36e63169
revandrazm <mahartarevandra@gmail.com>
2026-05-12 21:09:01 +0700

    feat: add giliranSelanjutnya function

--- src/ambilKartu.pl
+++ src/ambilKartu.pl
@@ -11,17 +11,32 @@ ambilKartu :-
      Jumlah = 1),
 
     ambilSejumlahKartu(Jumlah, KartuBaru),
-    append(KartuSebelum, KartuBaru, KartuSesudah),
+    append(KartuSebelum, KartuBaru, KartuSesudah),      % Ilegal
 
     retract(kartu_pemain(Pemain, KartuSebelum)),
     asserta(kartu_pemain(Pemain, KartuSesudah)),
 
-    format('Kartu ~w telah diperbarui. Total kartu sekarang: ~w~n', [Pemain, KartuSesudah]).
+    format('Kartu ~w telah diperbarui. Kartu sekarang: ~w~n', [Pemain, KartuSesudah]),
+
+    giliranSelanjutnya.
 
 ambilSejumlahKartu(0, []) :- !.
 ambilSejumlahKartu(Jumlah, [Kartu|Sisa]) :-
     loadKartu(DekKartu),
     random(0, 54, IndeksPilih),
-    nth0(IndeksPilih, DekKartu, Kartu),
+    nth0(IndeksPilih, DekKartu, Kartu),         % Ilegal
     JumlahBaru is Jumlah - 1,
     ambilSejumlahKartu(JumlahBaru, Sisa).
+
+giliranSelanjutnya :-
+    giliran(PemainSekarang),
+    urutan_pemain(DaftarPemain),
+
+    nth0(IndexLama, DaftarPemain, PemainSekarang),    % Ilegal
+    length(DaftarPemain, JumlahPemain),               % Ilegal
+    IndexBaru is (IndexLama + 1),
+
+    nth0(IndexBaru, DaftarPemain, PemainBerikutnya),
+
+    retract(giliran(PemainSekarang)),
+    asserta(giliran(PemainBerikutnya)).
