5618df4d16de117c5512d50274b59b4d63065d49
Muhammad Abduh <mabduh1357@gmail.com>
2026-05-12 20:02:54 +0700

    feat : menyelesaikan set up permainan

--- src/main.pl
+++ src/main.pl
@@ -1,20 +1,35 @@
 :- include('facts.pl').
 :- include('deck.pl').
 :- include('player.pl').
+:- dynamic(gameRunning/1).
+:- initialization(main).
+
+main :- startGame.
 
 startGame :-
+    assertz(gameRunning(true)),
+
     write('*******************************************'), nl,
     write('*         SELAMAT DATANG DI UNI!          *'), nl,
     write('*******************************************'), nl,  
 
     inputJumlahPemain(N),
-    inisialisasiPemain(N, ListPemain).
+    inisialisasiPemain(N, ListPemain),
+    loadKartu(Deck),
+    shuffle(Deck, DeckAcak),
+    bagiKartu(ListPemain, DeckAcak, SisaDeck),
+    initDiscard(SisaDeck, SisaDeckAkhir),
+    
+    assertz(deck(SisaDeckAkhir)),
+    assertz(arah_permainan(kanan)),
+
+    write('Set up selesai! Permainan dimulai!'), nl.
 
 inputJumlahPemain(N):-
     write('Masukkan jumlah pemain (2-4, akhiri dengan titik): '),
     read(Input),
     ( integer(Input), Input >= 2, Input =< 4 -> N = Input
     ;
-        write('Jumlah pemain tidak valid! Masukkan jumlah pemain lagi.')
+        write('Jumlah pemain tidak valid! Masukkan jumlah pemain lagi.'), nl,
         inputJumlahPemain(N)
     ).
