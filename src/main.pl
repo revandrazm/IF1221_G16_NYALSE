31aa1c8a517871297646a425e3f4a995fe2d3b44
Muhammad Abduh <mabduh1357@gmail.com>
2026-05-12 09:50:38 +0700

    feat : menambahkan start game dan input jumlah pemain

--- src/main.pl
+++ src/main.pl
@@ -1,7 +1,6 @@
 :- include('facts.pl').
 :- include('deck.pl').
 :- include('player.pl').
-
 startGame :-
     write('*******************************************'), nl,
     write('*         SELAMAT DATANG DI UNI!          *'), nl,
