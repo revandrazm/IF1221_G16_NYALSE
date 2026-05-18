277bc3d9375a6269042cab89cd93df7826573e6d
revandrazm <mahartarevandra@gmail.com>
2026-05-18 22:26:48 +0700

    refactor: use the correct function from utils.pl

--- src/ambilKartu.pl
+++ src/ambilKartu.pl
@@ -1,6 +1,6 @@
 :- include('facts.pl').
 :- include('deck.pl').
-:- include('helper.pl').
+:- include('utils.pl').
 
 ambilKartu :-
     giliran(Pemain),
@@ -12,7 +12,7 @@ ambilKartu :-
      Jumlah = 1),
 
     ambilSejumlahKartu(Jumlah, KartuBaru),
-    appendHelper(KartuSebelum, KartuBaru, KartuSesudah),
+    h_ListAppendList(KartuSebelum, KartuBaru, KartuSesudah),
 
     retract(kartu_pemain(Pemain, KartuSebelum)),
     asserta(kartu_pemain(Pemain, KartuSesudah)),
@@ -25,19 +25,20 @@ ambilSejumlahKartu(0, []) :- !.
 ambilSejumlahKartu(Jumlah, [Kartu|Sisa]) :-
     loadKartu(DekKartu),
     random(0, 54, IndeksPilih),
-    nth0Helper(IndeksPilih, DekKartu, Kartu),
+    h_ListGetElement(DekKartu, IndeksPilih, Kartu),
     JumlahBaru is Jumlah - 1,
     ambilSejumlahKartu(JumlahBaru, Sisa).
 
 giliranSelanjutnya :-
     giliran(PemainSekarang),
     urutan_pemain(DaftarPemain),
+    arah_permainan(Arah),
 
-    nth0Helper(IndexLama, DaftarPemain, PemainSekarang),
-    lengthHelper(DaftarPemain, JumlahPemain),
+    h_ListIndexOf(DaftarPemain, PemainSekarang, IndexLama),
+    h_ListLength(DaftarPemain, JumlahPemain),
     IndexBaru is (IndexLama + 1),
 
-    nth0Helper(IndexBaru, DaftarPemain, PemainBerikutnya),
+    h_ListGetElement(DaftarPemain, IndexBaru, PemainBerikutnya),
 
     retract(giliran(PemainSekarang)),
     asserta(giliran(PemainBerikutnya)).
