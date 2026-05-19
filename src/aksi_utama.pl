bd492855363227911ef05899b3c67d9510041e8e
revandrazm <mahartarevandra@gmail.com>
2026-05-19 10:05:54 +0700

    refactor: move ambilKartu and its helper function to aksi_utama

--- src/aksi_utama.pl
+++ src/aksi_utama.pl
@@ -1,11 +1,6 @@
-% :- include('facts.pl).
+:- include('facts.pl').
 :- include('utils.pl').
-:- dynamic(giliran/1).
-:- dynamic(kartuPemain/2).
-urutanPemain([1,2,3,4,5]).
-arah(1).
-giliran(1).
-kartuPemain(1,['hello', 'hi']).
+:- include('deck.pl').
 
 mainkanKartu(Idx) :-
     giliran(Pemain), urutanPemain(Urutan), h_ListIndexOf(Urutan, Pemain, IdPemain), h_ListLength(Urutan, NPemain), kartuPemain(Pemain, Deck), h_ListLength(Deck, N),
@@ -18,7 +13,50 @@ mainkanKartu(Idx) :-
     arah(Arah), IdPemain2 is (IdPemain+NPemain+Arah) mod NPemain,
     h_ListAtIndex(Urutan, IdPemain2, Pemain2), retract(giliran(_)), asserta(giliran(Pemain2)),
     format('Giliran ~w.', [Pemain2]), !.
-    
+
 kartuMainValid(kartu(Warna, Jenis)) :-
     discardTop(kartu(Warna2,Jenis2)),
     Warna=Warna2, Jenis=Jenis2.
+
+ambilKartu :-
+    giliran(Pemain),
+    kartu_pemain(Pemain, KartuSebelum),
+    discard_top(kartu(W, J)),
+
+    (J == drawTwo -> Jumlah = 2 ;
+     J == drawFour -> Jumlah = 4 ;
+     Jumlah = 1),
+
+    ambilSejumlahKartu(Jumlah, KartuBaru),
+    h_ListAppendList(KartuSebelum, KartuBaru, KartuSesudah),
+
+    retract(kartu_pemain(Pemain, KartuSebelum)),
+    asserta(kartu_pemain(Pemain, KartuSesudah)),
+
+    format('Kartu ~w telah diperbarui. Kartu sekarang: ~w~n', [Pemain, KartuSesudah]),
+
+    giliranSelanjutnya.
+
+ambilSejumlahKartu(0, []) :- !.
+ambilSejumlahKartu(Jumlah, [Kartu|Sisa]) :-
+    loadKartu(DekKartu),
+    random(0, 54, IndeksPilih),
+    h_ListGetElement(DekKartu, IndeksPilih, Kartu),
+    JumlahBaru is Jumlah - 1,
+    ambilSejumlahKartu(JumlahBaru, Sisa).
+
+giliranSelanjutnya :-
+    giliran(PemainSekarang),
+    urutan_pemain(DaftarPemain),
+    arah_permainan(Arah),
+
+    h_ListIndexOf(DaftarPemain, PemainSekarang, IndexLama),
+    h_ListLength(DaftarPemain, JumlahPemain),
+    (Arah == kanan ->
+     IndexBaru is (IndexLama + 1) mod JumlahPemain ;
+     IndexBaru is (IndexLama - 1 + JumlahPemain) mod JumlahPemain),
+
+    h_ListGetElement(DaftarPemain, IndexBaru, PemainBerikutnya),
+
+    retract(giliran(PemainSekarang)),
+    asserta(giliran(PemainBerikutnya)).
