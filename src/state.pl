422c90be0d2bcad2f8736b64591d712d532371f9
revandrazm <mahartarevandra@gmail.com>
2026-05-19 20:46:20 +0700

    feat: add efek_aktif

--- src/state.pl
+++ src/state.pl
@@ -1,11 +1,12 @@
 /* Fundamental Dynamics */
-:- dynamic(giliran/1).				% Nama pemain
-:- dynamic(urutanPemain/1).		% List nama []
-:- dynamic(arahPermainan/1).		% kanan/kiri (terpengaruh reverse)
-:- dynamic(discardTop/1).			% Kartu terakhir
-:- dynamic(warnaAktif/1).			% Bisa beda akibat wild
-:- dynamic(kartuPemain/2).			% Nama pemain, list kartu []
-:- dynamic(deck/1).					% Sisa kartu di deck
-:- dynamic(uniStatus/1).			% List pemain yg sudah uni
+:- dynamic(giliran/1).				  % Nama pemain
+:- dynamic(urutan_pemain/1).		% List nama []
+:- dynamic(arah_permainan/1).		% kanan/kiri (terpengaruh reverse)
+:- dynamic(discard_top/1).			% Kartu terakhir
+:- dynamic(warna_aktif/1).			% Bisa beda akibat wild
+:- dynamic(kartu_pemain/2).			% Nama pemain, list kartu []
+:- dynamic(deck/1).					    % Sisa kartu di deck
+:- dynamic(uni_status/1).			  % List pemain yg sudah uni
+:- dynamic(efek_aktif/1).       % Efek yang sedang aktif (skip, reverse, drawTwo, drawFour)
 
 /* Bonus */
