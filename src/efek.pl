a7c269272a4f880bcbf759201133d6c2b55d9441
revandrazm <mahartarevandra@gmail.com>
2026-05-19 20:54:19 +0700

    feat: add aplikasiEfek for skip, reverse, drawTwo, wildDrawFour, and default

---
+++ src/efek.pl
@@ -0,0 +1,30 @@
+:- include('state.pl').
+:- include('utils.pl').
+:- include('turn.pl').
+:- include('action.pl').
+
+aplikasiEfek(skip) :-
+    giliranSelanjutnya, !.
+
+aplikasiEfek(reverse) :-
+    arah_permainan(ArahAwal),
+
+    (ArahAwal == kanan ->
+     ArahBaru = kiri ;
+     ArahBaru = kanan),
+
+    retract(arah_permainan(ArahAwal)),
+    asserta(arah_permainan(ArahBaru)),
+
+    giliranSelanjutnya, !.
+
+aplikasiEfek(drawTwo) :-
+    ambilKartu,
+    giliranSelanjutnya, !.
+
+aplikasiEfek(wildDrawFour) :-
+    ambilKartu,
+    giliranSelanjutnya, !.
+
+aplikasiEfek(_):
+    giliranSelanjutnya.
