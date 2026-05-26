23ab54c7fd84c8db833689b8877f3e52d36a7c7b
Rempaah <rmuhammadreff@gmail.com>
2026-05-26 08:55:57 +0700

    feat: added player cards in save file

--- src/saveLoad.pl
+++ src/saveLoad.pl
@@ -6,29 +6,43 @@
 :- dynamic(arah/1).
 :- dynamic(kartuPemain/2).
 
-urutanPemain([1,2,3,4,5,6]).
-giliran(1).
+urutanPemain(['1','2','3','4','5','AB']).
+giliran('1').
 discard_top(kartu('merah','skip')).
 arah_permainan('kanan').
-kartuPemain(1, [kartu(1,2), kartu(2,3), kartu(3,4)]).
-kartuPemain(2, [kartu(1,2), kartu(2,3), kartu(3,4)]).
-kartuPemain(3, [kartu(1,2)]).
-kartuPemain(4, [kartu(1,2)]).
-kartuPemain(5, [kartu(1,2), kartu(2,3), kartu(3,4)]).
-kartuPemain(6, [kartu(1,2)]).
+kartuPemain('1', [kartu(1,2), kartu(2,3), kartu(3,4)]).
+kartuPemain('2', [kartu(1,2), kartu(2,3), kartu(3,4)]).
+kartuPemain('3', [kartu(1,2)]).
+kartuPemain('4', [kartu(1,2)]).
+kartuPemain('5', [kartu(1,2), kartu(2,3), kartu(3,4)]).
+kartuPemain('AB', [kartu(1,2)]).
 
 saveGame :-
   write('Masukkan nama file penyimpanan: '), read(X), nl,
   open(X, write, Stream),
-  urutanPemain(Urutan), format(Stream, 'urutan_pemain:~w.~n', [Urutan]),
-  giliran(Giliran), format(Stream, 'giliran:~w.~n', [Giliran]),
+  urutanPemain(Urutan), format(Stream, 'urutan_pemain:\'~w\'.~n', [Urutan]),
+  giliran(Giliran), format(Stream, 'giliran:\'~w\'.~n', [Giliran]),
   discard_top(kartu(Warna,Jenis)), format(Stream, 'discard_top:~w-~w.~n', [Warna, Jenis]),
   format(Stream, 'warna_aktif:~w.~n', [Warna]),
   arah_permainan(Arah), format(Stream, 'arah_permainan:~w.~n', [Arah]),
   pemainUNI(PemainUni), format(Stream, 'status_UNI:~w.~n', [PemainUni]),
+  writeSemuaKartuPemain(Urutan, Stream),
   close(Stream),
 
   format('Status permainan berhasil disimpan ke ~w.txt', [X]).
+
+loadGame :-
+  write('Masukkan nama file penyimpanan: '), read(X), nl,
+  open(X, read, Stream),
+  read(Stream, Term),
+  format('~w', [Term]).
+
+writeSemuaKartuPemain([], _Stream).
+writeSemuaKartuPemain([H|T], Stream) :-
+  writeKartuPemain(H, Stream), writeSemuaKartuPemain(T, Stream).
+
+writeKartuPemain(Pemain, Stream) :-
+  kartuPemain(Pemain, DaftarKartu), format(Stream, 'kartu(\'~w\'):~w.~n', [Pemain, DaftarKartu]).
 
 pemainUNI(Result) :- urutanPemain(Urutan), pemainUNI(Urutan, Result).
 
