80c25b7eba6e2250721ab18fcaae763a417a0832
revandrazm <mahartarevandra@gmail.com>
2026-05-31 02:07:39 +0700

    feat: menambahkan efek ketika dimainka kartu wild

--- src/efek.pl
+++ src/efek.pl
@@ -1,5 +1,6 @@
 :- include('state.pl').
 :- include('turn.pl').
+:- include('rule.pl').
 
 aplikasiEfek(skip) :-
     giliranSelanjutnya, !.
@@ -17,6 +18,28 @@ aplikasiEfek(reverse) :-
 aplikasiEfek(drawTwo) :-
     giliranSelanjutnya,
     ambilKartu, !.
+
+aplikasiEfek(wild) :-
+    write('Pilih warna (gunakan pilihWarna(Warna)): '),
+    retract(memilihWarna(false)),
+    asserta(memilihWarna(true)).
+
+pilihWarna(Warna) :-
+    memilihWarna(true), !,
+    (   cekWarnaValid(Warna)
+    ->  retract(memilihWarna(true)),
+        asserta(memilihWarna(false)),
+
+        retract(warnaAktif(_)),
+        asserta(warnaAktif(Warna)),
+
+        format('Berhasil! Warna permainan sekarang menjadi ~w.~n', [Warna]),
+    ;   write('Warna tidak valid! Silakan pilih: merah, kuning, hijau, atau biru.'), nl,
+        fail
+    ).
+
+pilihWarna(_) :-
+    write('Anda tidak sedang dalam fase memilih warna!'), nl.
 
 aplikasiEfek(wildDrawFour) :-
     giliranSelanjutnya,
